f7fe70e1871203b72f2fe47f4cf29ae470adeda3
sobiiya <ansarisobi2210@gmail.com>
2025-08-30 16:53:33 +0530

    todo completed using redux

---
+++ 10-reducToolkitTodo/src/App.jsx
@@ -0,0 +1,13 @@
+import AddTodo from "./components/addTodo";
+import Todo from "./components/todo";
+
+function App() {
+  return (
+    <>
+  <AddTodo/>
+  <Todo />
+    </>
+  );
+}
+
+export default App;
